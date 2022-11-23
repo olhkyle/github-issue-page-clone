f1b2b2982502ac092818325cd284fa51a96d5bde
olhkyle <kylekwon96@gmail.com>
2022-11-24 00:49:52 +0900

    Feat : section 컴포넌트에 input 검색 기능 추가

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import React, { useState } from 'react'
 import Button from './commons/Button'
 import { ButtonProps } from './Header'
 
@@ -8,9 +9,18 @@ const Section = () => {
     fontColor: 'white',
     fontSize: '14px',
   }
+
+  const [inputValue, setInputValue] = useState<string>('is:pr is:open')
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
   return (
     <ListContainer>
-      <Button issueBtn={issueBtn}>New Issue</Button>
+      <Wrapper>
+        <Input value={inputValue} type="text" onChange={handleInputChange} />
+        <Button issueBtn={issueBtn}>New Issue</Button>
+      </Wrapper>
     </ListContainer>
   )
 }
@@ -21,4 +31,19 @@ const ListContainer = styled.div`
   padding: 0 2rem;
   margin-top: 24px;
   height: 220px;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Input = styled.input`
+  margin-right: auto;
+  padding-left: 10px;
+  width: 60%;
+  border-radius: 6px;
+  border: 1px solid #d0d7de;
+  color: #57606a;
+  background: #f6f8fa;
 `
